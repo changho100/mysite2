37925ce772fc1c64ca95eda1afcb67ca479316a6
changho100 <chbeak@naver.com>
2023-10-17 18:37:39 +0900

    mysite2

---
+++ nirs/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'nirs/index.html')
+
+
+def exe_search_html(request):
+    
+    # html_name = request.POST['html_name']
+
+    html_name = "sample.html"
+    
+    print(html_name)
+
+    context = { 'html_name' : html_name}
+    
+    return render(request, 'nirs/test1.html', context)
