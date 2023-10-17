7023ba04f2123031e07d37147744cdb43ed3aa7a
changho100 <chbeak@naver.com>
2023-10-17 18:49:44 +0900

    3

--- nirs/urls.py
+++ nirs/urls.py
@@ -1,5 +1,4 @@
-from os import path
-
+from django.urls import path
 from . import views
 
 urlpatterns = [
--- nirs/views.py
+++ nirs/views.py
@@ -16,4 +16,4 @@ def exe_search_html(request):
 
     context = { 'html_name' : html_name}
     
-    return render(request, 'nirs/index.html', context)
+    return render(request, 'nirs/html_view.html', context)
